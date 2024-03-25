72c2aa13107ee72bf019801e389b3996ed93e493
Jhankda <harshjhankda200988@gmail.com>
2024-03-25 22:08:33 +0530

    conversions

---
+++ 01-basejs/03-conversions.js
@@ -0,0 +1,31 @@
+var score = 16.235
+
+console.log(typeof score)
+console.log(typeof(score))
+
+var score = Number(score)
+
+
+console.log(typeof(score))
+
+console.log(score + score)
+
+
+
+// in conversions
+// true => 1
+// "33" => 33
+// "33abc" => NaN
+
+let isLoggedin = ""
+let boolIsLoggedIn = Boolean(isLoggedin)
+console.log(boolIsLoggedIn)
+
+let someNumber = 99
+let isString = String(someNumber)
+console.log(typeof(isString))
+
+
+// ********************opeerations*****************
+
+
