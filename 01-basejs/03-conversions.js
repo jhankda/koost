2d21ed8212cc42a0b4c410cc3d92859d8175cfa7
Jhankda <harshjhankda200988@gmail.com>
2024-03-25 22:32:48 +0530

    operatins and convrsions

--- 01-basejs/03-conversions.js
+++ 01-basejs/03-conversions.js
@@ -1,14 +1,14 @@
-var score = 16.235
+// var score = 16.235
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
-var score = Number(score)
+// var score = Number(score)
 
 
-console.log(typeof(score))
+// console.log(typeof(score))
 
-console.log(score + score)
+// console.log(score + score)
 
 
 
@@ -17,15 +17,32 @@ console.log(score + score)
 // "33" => 33
 // "33abc" => NaN
 
-let isLoggedin = ""
-let boolIsLoggedIn = Boolean(isLoggedin)
-console.log(boolIsLoggedIn)
+// let isLoggedin = ""
+// let boolIsLoggedIn = Boolean(isLoggedin)
+// console.log(boolIsLoggedIn)
 
-let someNumber = 99
-let isString = String(someNumber)
-console.log(typeof(isString))
+// let someNumber = 99
+// let isString = String(someNumber)
+// console.log(typeof(isString))
 
 
 // ********************opeerations*****************
 
+// let value = 3
+// let negvalue = -value
+// console.log(negvalue)
+// all basic maths can be applied
+// console.log(3**3)
 
+// let str1 = "hello"
+// let str2 = "harsh"
+
+// console.log(str1 + str2)
+// console.log(typeof(1 + str1)) => jab tak string nahi aata tab tak concatination nahi start hota
+
+
+// console.log(+true)
+// console.log(+"")
+
+let gamecount = 100
+console.log(gamecount++)
