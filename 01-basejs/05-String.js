be915475ed905718ed40db03cc12f700a9a6d8ff
Jhankda <harshjhankda200988@gmail.com>
2024-03-26 19:50:04 +0530

    string added

--- 01-basejs/05-String.js
+++ 01-basejs/05-String.js
@@ -1,1 +1,36 @@
- 
+ const name = "harsh"
+ const repocount = 50
+
+ console.log(name + repocount + "value");
+
+ console.log(`${name} has ${repocount} repos`)//called string interpolation
+
+ const gameName = new String("Modern-Warfare")
+
+ console.log(gameName.__proto__)
+
+ console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(9))
+console.log(gameName.indexOf("War"))
+
+const newString = gameName.substring(0,6)
+
+console.log(newString)
+
+const anotherString = gameName.slice(3,6)
+console.log(anotherString)
+
+const NewString = "   hello harsh   "
+console.log(NewString)
+console.log(NewString.trim())
+console.log("\n");
+
+const url = "https://www.google.com/harsh golem"
+
+url.replace(" ","-")
+console.log(url.replace(" ","-"))
+
+console.log(url.includes("harsh"))
+
+console.log(url.split("/"))
